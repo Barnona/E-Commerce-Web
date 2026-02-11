ad5ab561652b44b4bdbd285e867c32d78110bb8a
Barnona Das <147792242+Barnona@users.noreply.github.com>
2026-02-11 07:34:01 +0530

    Update CORS origin for the application

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors({
     origin: [
         "http://localhost:5173", 
         "http://localhost:5174",
-        "https://e-shop-es5i.onrender.com",
+        "https://https://eshop-5p90wb3lt-barnonas-projects.vercel.app",
         "https://your-admin-app.vercel.app"
     ],
     credentials: true
