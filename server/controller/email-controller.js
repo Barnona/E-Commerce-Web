783073dff0a88792a4bf98259273fa737b570b21
Barnona Das <147792242+Barnona@users.noreply.github.com>
2026-09-12 20:39:54 +0530

    Security: remove environment credential logging

--- server/controller/email-controller.js
+++ server/controller/email-controller.js
@@ -1,8 +1,8 @@
 const nodemailer = require('nodemailer');
-console.log("Email from ENV:", process.env.SMPT_MAIL);
-console.log("Password from ENV:", process.env.SMPT_PASSWORD ? "Loaded" : "Not Loaded");
 
-// Configure your email service
+const requiredEnv = ['SMPT_MAIL', 'SMPT_PASSWORD'];
+const missingEnv = requiredEnv.filter((name) => !process.env[name]);
+
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -12,19 +12,18 @@ const transporter = nodemailer.createTransport({
 });
 
 const sendEmail = async (to, subject, text) => {
+    if (missingEnv.length) {
+        throw new Error(`Email service is not configured: missing ${missingEnv.join(', ')}`);
+    }
+
     const mailOptions = {
         from: process.env.SMPT_MAIL,
-        to: to,
-        subject: subject,
-        text: text
+        to,
+        subject,
+        text
     };
 
-    try {
-        await transporter.sendMail(mailOptions);
-        console.log('Email Sent Successfully');
-    } catch (error) {
-        console.log('Email Send Error:', error);
-    }
+    await transporter.sendMail(mailOptions);
 };
 
 module.exports = { sendEmail };
